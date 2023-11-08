c075b151bf9ceffaad9f0ce6325fa331444dcab7
Francielle Abreu da Silva <fran.abreu2@gmail.com>
2023-11-08 10:17:36 +0100

    Wednesday Assignment

--- Week-2/3-sentence.js
+++ Week-2/3-sentence.js
@@ -8,3 +8,26 @@
 // titleCase("I'm a little tea pot")should return a string.
 // titleCase("I'm a little tea pot")should return "I'm A Little Tea Pot".
 // titleCase("sHoRt AnD sToUt") should return "Short And Stout".
+
+// Pseudocode
+// Function
+// Use split
+// For loop first word capitalized 
+// Use method for the result
+// Print it
+
+function titleCase(str) {
+    let words = str.toLowerCase().split(" ");
+
+    for (var i = 0; i < words.length; i++) {
+        let word = words[i];
+        words[i] = word.charAt(0).toUpperCase() + word.slice(1);
+    }
+
+    const result = words.join(" ");
+
+    return result;
+}
+
+console.log(titleCase("I'm a little tea pot"));  // I'm A Little Tea Pot
+console.log(titleCase("sHoRt AnD sToUt"));  // Short And Stout
